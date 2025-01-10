d6c5af0a3fd89d515c70b036f65eedcc1cc8ffdd
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-01-10 19:01:05 +0000

    Removed Django secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from decouple import config
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-15jk8e6gt7fby$vc3s2!j8dhfx9%mg7d_3_@585t6yhjsf_kj="
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
