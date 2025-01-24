3a7d5fea9778fa8e27c954bdd3ed212423137691
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-01-24 17:50:08 +0000

    First commit

--- frontend/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import api from "../api";
-
-export default function Home() {
-    const getAllItems = api.get(process.env.REACT_APP_URL + "/api/items/");
-    getAllItems.then(data => console.log(data.data))
-    return <p>Home page</p>
-}
