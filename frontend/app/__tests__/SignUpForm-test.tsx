b8be3f71fa6fb7486533ec1ddb160e2456b777d7
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-01-26 00:00:39 +0000

    Frontend - created and styled sign up form

---
+++ frontend/app/__tests__/SignUpForm-test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react-native";
+
+import SignUpForm from "../sign-up/index";
+
+describe("<SignUpForm />", () => {
+  beforeEach(() => {
+    render(<SignUpForm />);
+  });
+  test("Should render 'Sign up' text", () => {
+    screen.getByText("Sign up");
+  });
+  test("Should render 'Username' text", () => {
+    screen.getByText("Username");
+  });
+  test("Should render text input for Username", () => {
+    screen.getByLabelText("Username text input");
+  });
+  test("Should render username requirements text", () => {
+    screen.getByText(
+      "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+    );
+  });
+  test("Should render 'Password' text", () => {
+    screen.getByText("Password");
+  });
+  test("Should render text input for Password", () => {
+    screen.getByLabelText("Password input");
+  });
+  test("Should render password requirements text", () => {
+    screen.getByTestId("password-requirements");
+  });
+
+  test("Should render Submit button", () => {
+    screen.getByLabelText("Form submit button");
+  });
+
+  test("Should render 'Already a customer? Sign in' text", () => {
+    screen.getByText("Already a customer? Sign in")
+  })
+});
---
+++ frontend/app/sign-up/index.tsx
@@ -0,0 +1,60 @@
+import { FlatList, Text, TextInput, View } from "react-native";
+import Button from "../components/SubmitButton";
+
+const DATA = [
+    "Password can't be too similar to your other personal information",
+    "Password must contain at least 8 characters",
+    "Password can't be a commonly used password",
+    "Password can't be entirely numeric",
+];
+
+export default function SignUpForm() {
+    return (
+        <View className="flex-1 bg-[#e0b688] px-[20px] pt-[40px]">
+            <Text className="text-center font-serif text-3xl font-bold mb-[10px]">
+                Sign up
+            </Text>
+            <Text className="text-center font-serif text-base font-bold text-[#787171] mb-[20px]">
+                Create an account to add items to your basket
+            </Text>
+
+            <View className="mb-[20px]">
+                <Text className="font-serif text-[21px] font-bold mb-[5px]">
+                    Username
+                </Text>
+                <TextInput
+                    accessibilityLabel="Username text input"
+                    className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
+                    inputMode="text"
+                />
+                <Text className="font-serif text-sm text-[#787171] mb-[5px] font-bold">
+                    Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.
+                </Text>
+
+                <Text className="font-serif text-[21px] font-bold mb-[5px]">
+                    Password
+                </Text>
+                <TextInput
+                    accessibilityLabel="Password input"
+                    className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
+                    secureTextEntry
+                    inputMode="text"
+                />
+                <FlatList
+                    data={DATA}
+                    renderItem={({ item }) => (
+                        <Text className="font-serif text-sm text-[#787171] mb-[5px] font-bold">
+                            {item}
+                        </Text>
+                    )}
+                    testID="password-requirements"
+                />
+            </View>
+
+            <Button />
+            <Text className="text-center font-serif text-base font-bold mt-[20px]">
+                Already a customer? Sign in
+            </Text>
+        </View>
+    );
+}
