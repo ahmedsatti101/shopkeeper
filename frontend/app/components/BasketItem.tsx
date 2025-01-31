3d2f90f4bf26671ff6c0c404c3c5cc4c1d800efd
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-01-31 19:54:56 +0000

    Created dropdown list for selecting quantity of items in the basket

--- frontend/app/components/BasketItem.tsx
+++ frontend/app/components/BasketItem.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Image, Pressable, Text, View } from "react-native";
+import Dropdown from "./Dropdown";
 const image = require("../../assets/images/download.jpeg");
 const trash = require("../../assets/images/trash.png");
 
@@ -7,25 +9,41 @@ interface Props {
     price: any;
 }
 
-export default function BasketItem({title, price}: Props) {
-  return (
-    <>
-      <View className="bg-[#cc9861] flex-row shadow-[0px_0px_30px_0px_rgba(0,0,0,0.40)] items-center m-4">
-        <Image alt="Image" source={image} style={{ width: 120, height: 133 }} />
-        <View className="px-4 pb-20">
-          <Text className="font-serif font-bold text-lg">{title}</Text>
-          <Text className="font-serif text-lg">£{price}</Text>
-        </View>
-        <Pressable onPress={() => console.log("trash")} className="right-0 bottom-0 absolute">
-          <Image
-            source={trash}
-            style={{
-              width: 30,
-              height: 40,
-            }}
-          />
-        </Pressable>
-      </View>
-    </>
-  );
+const DATA = Array.from({ length: 20 }, (_, index) => ({
+    label: `${index + 1}`,
+    value: index + 1,
+}));
+
+export default function BasketItem({ title, price }: Props) {
+    const [quantity, setQuantity] = useState(1);
+
+    return (
+        <>
+            <View className="bg-[#cc9861] flex-row shadow-[0px_0px_30px_0px_rgba(0,0,0,0.40)] items-center m-4">
+                <Image alt="Image" source={image} style={{ width: 120, height: 133 }} />
+                <View className="px-4 pb-15">
+                    <Text className="font-serif font-bold text-lg">{title}</Text>
+                    <Text className="font-serif text-lg">£{price}</Text>
+                    <Dropdown
+                        data={DATA}
+                        placeholder="1"
+                        onChange={(item) => setQuantity(item.value)}
+                    />
+                </View>
+
+                <Pressable
+                    onPress={() => console.log("trash")}
+                    className="right-0 bottom-0 absolute"
+                >
+                    <Image
+                        source={trash}
+                        style={{
+                            width: 30,
+                            height: 40,
+                        }}
+                    />
+                </Pressable>
+            </View>
+        </>
+    );
 }
---
+++ frontend/app/components/Dropdown.tsx
@@ -0,0 +1,132 @@
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    FlatList,
+    Modal,
+    TouchableWithoutFeedback,
+    Platform,
+} from "react-native";
+import React, { useCallback, useRef, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+
+type OptionItem = {
+    value: number;
+    label: string;
+};
+
+interface DropDownProps {
+    data: OptionItem[];
+    onChange: (item: OptionItem) => void;
+    placeholder: string;
+}
+
+export default function Dropdown({
+    data,
+    onChange,
+    placeholder,
+}: DropDownProps) {
+    const [expanded, setExpanded] = useState(false);
+    const [value, setValue] = useState(1);
+    const buttonRef = useRef<View>(null);
+    const [top, setTop] = useState(0);
+
+    const toggleExpanded = useCallback(() => setExpanded(!expanded), [expanded]);
+
+    const onSelect = useCallback((item: OptionItem) => {
+        onChange(item);
+        setValue(item.value);
+        setExpanded(false);
+    }, []);
+
+    const onButtonLayout = (event: any) => {
+        const layout = event.nativeEvent.layout;
+        const topOffset = layout.y;
+        const heightOfComponent = layout.height;
+
+        const finalValue =
+            topOffset + heightOfComponent + (Platform.OS === "android" ? 0 : 0);
+
+        setTop(finalValue);
+    };
+
+    return (
+        <View ref={buttonRef} onLayout={onButtonLayout}>
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.8}
+                onPress={toggleExpanded}
+            >
+                <Text style={styles.text}>{value || placeholder}</Text>
+                <AntDesign name={expanded ? "caretup" : "caretdown"} />
+            </TouchableOpacity>
+            {expanded ? (
+                <Modal visible={expanded} transparent>
+                    <TouchableWithoutFeedback onPress={() => setExpanded(false)}>
+                        <View style={styles.backdrop}>
+                            <View style={[styles.options, { top }]}>
+                                <FlatList
+                                    style={{maxHeight: 200}}
+                                    keyExtractor={(item) => item.value}
+                                    data={data}
+                                    renderItem={({ item }) => (
+                                        <TouchableOpacity
+                                            activeOpacity={0.8}
+                                            style={styles.optionItem}
+                                            onPress={() => onSelect(item)}
+                                        >
+                                            <Text>{item.label}</Text>
+                                        </TouchableOpacity>
+                                    )}
+                                    ItemSeparatorComponent={() => (
+                                        <View style={styles.separator} />
+                                    )}
+                                />
+                            </View>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </Modal>
+            ) : null}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    backdrop: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.5)",
+    },
+    optionItem: {
+        height: 40,
+        justifyContent: "center",
+    },
+    separator: {
+        height: 1,
+        backgroundColor: "#ccc",
+    },
+    options: {
+        backgroundColor: "#fff",
+        width: 200,
+        padding: 10,
+        borderRadius: 6,
+        position: "absolute",
+    },
+    text: {
+        fontSize: 15,
+        opacity: 0.8,
+    },
+    button: {
+        height: 40,
+        justifyContent: "space-between",
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        width: 90,
+        alignItems: "center",
+        paddingHorizontal: 10,
+        borderRadius: 7,
+        borderWidth: 1,
+    },
+});
