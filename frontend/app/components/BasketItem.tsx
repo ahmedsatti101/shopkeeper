b31ab5a7673dbc1029982e5c4667d33dcfc2fb69
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-01-31 21:00:25 +0000

    Finished creating the final UI for basket page

--- frontend/app/components/BasketItem.tsx
+++ frontend/app/components/BasketItem.tsx
@@ -1,49 +1,51 @@
 import { useState } from "react";
 import { Image, Pressable, Text, View } from "react-native";
 import Dropdown from "./Dropdown";
+import BasketSummary from "./BasketSummary";
 const image = require("../../assets/images/download.jpeg");
 const trash = require("../../assets/images/trash.png");
 
 interface Props {
-    title: string;
-    price: any;
+  title: string;
+  price: any;
 }
 
 const DATA = Array.from({ length: 20 }, (_, index) => ({
-    label: `${index + 1}`,
-    value: index + 1,
+  label: `${index + 1}`,
+  value: index + 1,
 }));
 
 export default function BasketItem({ title, price }: Props) {
-    const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
-    return (
-        <>
-            <View className="bg-[#cc9861] flex-row shadow-[0px_0px_30px_0px_rgba(0,0,0,0.40)] items-center m-4">
-                <Image alt="Image" source={image} style={{ width: 120, height: 133 }} />
-                <View className="px-4 pb-15">
-                    <Text className="font-serif font-bold text-lg">{title}</Text>
-                    <Text className="font-serif text-lg">£{price}</Text>
-                    <Dropdown
-                        data={DATA}
-                        placeholder="1"
-                        onChange={(item) => setQuantity(item.value)}
-                    />
-                </View>
+  return (
+    <>
+      <View className="bg-[#cc9861] flex-row shadow-[0px_0px_30px_0px_rgba(0,0,0,0.40)] items-center m-4">
+        <Image alt="Image" source={image} style={{ width: 120, height: 133 }} />
+        <View className="px-4 pb-15">
+          <Text className="font-serif font-bold text-lg">{title}</Text>
+          <Text className="font-serif text-lg">£{price}</Text>
+          <Dropdown
+            data={DATA}
+            placeholder="1"
+            onChange={(item) => setQuantity(item.value)}
+          />
+        </View>
 
-                <Pressable
-                    onPress={() => console.log("trash")}
-                    className="right-0 bottom-0 absolute"
-                >
-                    <Image
-                        source={trash}
-                        style={{
-                            width: 30,
-                            height: 40,
-                        }}
-                    />
-                </Pressable>
-            </View>
-        </>
-    );
+        <Pressable
+          onPress={() => console.log("trash")}
+          className="right-0 bottom-0 absolute"
+        >
+          <Image
+            source={trash}
+            style={{
+              width: 30,
+              height: 40,
+            }}
+          />
+        </Pressable>
+      </View>
+      <BasketSummary price="3.20" quantity={quantity} />
+    </>
+  );
 }
