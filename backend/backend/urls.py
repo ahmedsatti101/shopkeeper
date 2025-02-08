d29aa09713ef38137a3ac13a789913f6c1f3e632
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-02-08 15:09:11 +0000

    Backend - added image field to Item model

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +16,4 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'),
          name='swagger-ui')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
