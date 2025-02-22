07dd1f2529ecd35fd442ed2dc734d4d6dbcee531
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-02-22 13:05:02 +0000

    Frontend - added form validation to sign up and in forms

--- frontend/app/sign-in/index.tsx
+++ frontend/app/sign-in/index.tsx
@@ -1,35 +1,81 @@
 import { Text, View, TextInput } from "react-native";
 import Button from "../components/SubmitButton";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRef } from "react";
+
+const formSchema = yup.object({
+  username: yup.string().required("Username is required").default(""),
+  password: yup.string().required("Password is required").default(""),
+});
 
 export default function SignInForm() {
-    return (
-        <View className="flex-1 bg-[#e0b688] px-[20px] pt-[40px]">
-            <Text className="text-center font-serif text-3xl font-bold mb-[10px]">
-                Sign in
-            </Text>
-            <View className="mb-[20px]">
-                <Text className="font-serif text-[21px] font-bold mb-[5px]">
-                    Username
-                </Text>
-                <TextInput
-                    accessibilityLabel="Username text input"
-                    className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
-                    inputMode="text"
-                />
-                <Text className="font-serif text-[21px] font-bold mb-[5px]">
-                    Password
-                </Text>
-                <TextInput
-                    accessibilityLabel="Password input"
-                    className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
-                    secureTextEntry
-                    inputMode="text"
-                />
-                <Button text="Sign in"/>
-                <Text className="text-center font-serif text-base font-bold mt-[20px]">
-                    Need to create an account? Sign up
-                </Text>
-            </View>
-        </View>
-    );
+  const passwordInputRef = useRef(null);
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({ resolver: yupResolver(formSchema) });
+
+  const onSubmit: SubmitHandler<typeof formSchema> = (data: any) =>
+    console.log(data);
+
+  return (
+    <View className="flex-1 bg-[#e0b688] px-[20px] pt-[40px]">
+      <Text className="text-center font-serif text-3xl font-bold mb-[10px]">
+        Sign in
+      </Text>
+      <View className="mb-[20px]">
+        <Text className="font-serif text-[21px] font-bold mb-[5px]">
+          Username
+        </Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              accessibilityLabel="Username text input"
+              className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
+              inputMode="text"
+              onChangeText={onChange}
+              value={value}
+              returnKeyType="next"
+              onSubmitEditing={() => passwordInputRef.current.focus()}
+            />
+          )}
+          name="username"
+        />
+        {errors.username?.message && (
+          <Text className="text-red-600 m-1">{errors.username?.message}</Text>
+        )}
+
+        <Text className="font-serif text-[21px] font-bold mb-[5px]">
+          Password
+        </Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              ref={passwordInputRef}
+              accessibilityLabel="Password input"
+              className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
+              secureTextEntry
+              inputMode="text"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="password"
+        />
+        {errors.password?.message && (
+          <Text className="text-red-600 m-1">{errors.password?.message}</Text>
+        )}
+        <Button text="Sign in" onPress={handleSubmit(onSubmit)} />
+        <Text className="text-center font-serif text-base font-bold mt-[20px]">
+          Need to create an account? Sign up
+        </Text>
+      </View>
+    </View>
+  );
 }
--- frontend/app/sign-up/index.tsx
+++ frontend/app/sign-up/index.tsx
@@ -1,96 +1,122 @@
 import { FlatList, Text, TextInput, View } from "react-native";
-import { object, string } from "yup";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import Button from "../components/SubmitButton";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRef } from "react";
 
-const DATA = [
-    "Password can't be too similar to your other personal information",
-    "Password must contain at least 8 characters",
-    "Password can't be a commonly used password",
-    "Password can't be entirely numeric",
+const USRNAMEREQS = [
+  "Username can be 150 characters or less",
+  "Letters, digits and @ . + - _ characters can only be used",
 ];
 
-const formSchema = object({
-    username: string()
-        .required("Username is required")
-        .max(150, "Username length must be 150 characters or less"),
-    password: string()
-        .required("Password is required")
-        .min(8, "Password must be at least 8 characters"),
+const PASSREQ = [
+  "Password can't be too similar to your other personal information",
+  "Password must contain at least 8 characters",
+  "Password can't be a commonly used password",
+  "Password can't be entirely numeric",
+];
+
+const formSchema = yup.object({
+  username: yup
+    .string()
+    .required("Username is required")
+    .max(150, "Username length can only be 150 characters or less")
+    .default(""),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .default(""),
 });
 
 export default function SignUpForm() {
-    const onSubmit = () => {
-        console.log("form submitted");
-    };
-    return (
-        <View className="flex-1 bg-[#e0b688] px-[20px] pt-[40px]">
-            <Text className="text-center font-serif text-3xl font-bold mb-[10px]">
-                Sign up
+  const passwordInputRef = useRef(null);
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({ resolver: yupResolver(formSchema) });
+
+  const onSubmit: SubmitHandler<typeof formSchema> = (data: any) =>
+    console.log(data);
+
+  return (
+    <View className="flex-1 bg-[#e0b688] px-[20px] pt-[40px]">
+      <Text className="text-center font-serif text-3xl font-bold mb-[10px]">
+        Sign up
+      </Text>
+
+      <View className="mb-[20px]">
+        <Text className="font-serif text-[21px] font-bold mb-[5px]">
+          Username
+        </Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              accessibilityLabel="Username text input"
+              className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
+              inputMode="text"
+              value={value}
+              onChangeText={onChange}
+              returnKeyType="next"
+              onSubmitEditing={() => passwordInputRef.current.focus()}
+            />
+          )}
+          name="username"
+        />
+        {errors.username?.message && (
+          <Text className="text-red-600 m-1">{errors.username?.message}</Text>
+        )}
+
+        <FlatList
+          data={USRNAMEREQS}
+          renderItem={({ item }) => (
+            <Text className="font-serif text-sm text-[#787171] mb-[5px] font-bold">
+              {item}
             </Text>
-            <Text className="text-center font-serif text-base font-bold text-[#787171] mb-[20px]">
-                Create an account to add items to your basket
+          )}
+          testID="password-requirements"
+        />
+
+        <Text className="font-serif text-[21px] font-bold mb-[5px]">
+          Password
+        </Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              ref={passwordInputRef}
+              accessibilityLabel="Password input"
+              className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
+              secureTextEntry
+              inputMode="text"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="password"
+        />
+        {errors.password?.message && (
+          <Text className="text-red-600 m-1">{errors.password?.message}</Text>
+        )}
+        <FlatList
+          data={PASSREQ}
+          renderItem={({ item }) => (
+            <Text className="font-serif text-sm text-[#787171] mb-[5px] font-bold">
+              {item}
             </Text>
+          )}
+          testID="password-requirements"
+        />
+      </View>
 
-            <View className="mb-[20px]">
-                <Formik
-                    initialValues={{ username: "", password: "" }}
-                    validationSchema={formSchema}
-                    onSubmit={onSubmit}
-                >
-                    {({ isSubmitting, isValid, errors }) => (
-                        <Form>
-                            <Text className="font-serif text-[21px] font-bold mb-[5px]">
-                                Username
-                            </Text>
-                            <Field name="username">
-                                {({ field, form }) => (
-                                    <input
-                                        className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
-                                        type="text"
-                                    />
-                                )}
-                            </Field>
-                            {errors.username}
-                            <Text className="font-serif text-sm text-[#787171] mb-[5px] font-bold">
-                                Required. 150 characters or fewer. Letters, digits and @/./+/-/_
-                                only.
-                            </Text>
-
-                            <Text className="font-serif text-[21px] font-bold mb-[5px]">
-                                Password
-                            </Text>
-                            <Field name="password">
-                                {({ field, form }) => (
-                                    <input
-                                        className="rounded-[10px] bg-[#c4574f40] px-10 mb-[10px] h-[45px]"
-                                        type="password"
-                                    />
-                                )}
-                            </Field>
-                            {errors.password}
-                            <FlatList
-                                data={DATA}
-                                renderItem={({ item }) => (
-                                    <Text className="font-serif text-sm text-[#787171] mb-[5px] font-bold">
-                                        {item}
-                                    </Text>
-                                )}
-                                testID="password-requirements"
-                            />
-
-                            <button type="submit" disabled={isSubmitting || !isValid}>
-                                Sign up
-                            </button>
-                        </Form>
-                    )}
-                </Formik>
-            </View>
-
-            {/* <Button text="Sign up" /> */}
-            <Text className="text-center font-serif text-base font-bold mt-[20px]">
-                Already a customer? Sign in
-            </Text>
-        </View>
-    );
+      <Button text="Sign up" onPress={handleSubmit(onSubmit)} />
+      <Text className="text-center font-serif text-base font-bold mt-[20px]">
+        Already a customer? Sign in
+      </Text>
+    </View>
+  );
 }
