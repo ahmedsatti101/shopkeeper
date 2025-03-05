1cdb090e8c335c9c32558d131e0ea638b3cc5935
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-03-05 13:19:38 +0000

    Frontend - fixed test which asserts user is navigated to sign in form after signing up

--- frontend/app/__tests__/SignUpForm-test.tsx
+++ frontend/app/__tests__/SignUpForm-test.tsx
@@ -5,100 +5,106 @@ import { userEvent } from "@testing-library/react-native";
 import SignUpForm from "../sign-up/index";
 
 describe("<SignUpForm />", () => {
-    beforeEach(() => {
-        render(<SignUpForm />);
-    });
+  beforeEach(() => {
+    render(<SignUpForm />);
+  });
 
-    test("Should render 'Sign up' text", () => {
-        screen.getAllByText("Sign up");
-    });
-    test("Should render 'Username' text", () => {
-        screen.getByText("Username");
-    });
-    test("Should render text input for Username", () => {
-        screen.getByLabelText("Username text input");
-    });
-    test("Should render username requirements text", () => {
-        screen.getByTestId("password-requirements");
-    });
-    test("Should render 'Password' text", () => {
-        screen.getByText("Password");
-    });
-    test("Should render text input for Password", () => {
-        screen.getByLabelText("Password input");
-    });
-    test("Should render password requirements text", () => {
-        screen.getByTestId("password-requirements");
-    });
-    test("Should render Submit button", () => {
-        screen.getByLabelText("Form submit button");
-    });
-    test("Should render 'Already a customer? Sign in' text", () => {
-        screen.getByText("Already a customer? Sign in");
-    });
-    test("Should render form error messages for submitting empty form", async () => {
-        const user = userEvent.setup();
-        const button = screen.getByTestId("submit-button");
+  test("Should render 'Sign up' text", () => {
+    screen.getAllByText("Sign up");
+  });
+  test("Should render 'Username' text", () => {
+    screen.getByText("Username");
+  });
+  test("Should render text input for Username", () => {
+    screen.getByLabelText("Username text input");
+  });
+  test("Should render username requirements text", () => {
+    screen.getByTestId("password-requirements");
+  });
+  test("Should render 'Password' text", () => {
+    screen.getByText("Password");
+  });
+  test("Should render text input for Password", () => {
+    screen.getByLabelText("Password input");
+  });
+  test("Should render password requirements text", () => {
+    screen.getByTestId("password-requirements");
+  });
+  test("Should render Submit button", () => {
+    screen.getByLabelText("Form submit button");
+  });
+  test("Should render 'Already a customer? Sign in' text", () => {
+    screen.getByText("Already a customer? Sign in");
+  });
+  test("Should render form error messages for submitting empty form", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByTestId("submit-button");
 
-        await user.press(button);
+    await user.press(button);
 
-        const userErrMsg = screen.getByText("Username is required");
-        const passwordErrMsg = screen.getByText("Password is required");
+    const userErrMsg = screen.getByText("Username is required");
+    const passwordErrMsg = screen.getByText("Password is required");
 
-        expect(userErrMsg).toBeOnTheScreen();
-        expect(passwordErrMsg).toBeOnTheScreen();
+    expect(userErrMsg).toBeOnTheScreen();
+    expect(passwordErrMsg).toBeOnTheScreen();
+  });
+  test("Password input value should match specified validation", async () => {
+    const user = userEvent.setup();
+    const passwordField = screen.getByLabelText("Password input");
+    const button = screen.getByTestId("submit-button");
+
+    await user.type(passwordField, "Testpss");
+    await user.press(button);
+
+    expect(
+      screen.getByText("Password must be at least 8 characters"),
+    ).toBeOnTheScreen();
+  });
+  describe("Field labels", () => {
+    test("Should render form label for Username field", () => {
+      const label = screen.getByRole("text", { name: "Username" });
+      expect(label).toBeOnTheScreen();
     });
-    test("Password input value should match specified validation", async () => {
-        const user = userEvent.setup();
-        const passwordField = screen.getByLabelText("Password input");
-        const button = screen.getByTestId("submit-button");
-
-        await user.type(passwordField, "Testpss");
-        await user.press(button);
-
-        expect(
-            screen.getByText("Password must be at least 8 characters"),
-        ).toBeOnTheScreen();
+    test("Should render form label for Password field", () => {
+      const label = screen.getByRole("text", { name: "Password" });
+      expect(label).toBeOnTheScreen();
     });
-    describe("Field labels", () => {
-        test("Should render form label for Username field", () => {
-            const label = screen.getByRole("text", { name: "Username" });
-            expect(label).toBeOnTheScreen();
-        });
-        test("Should render form label for Password field", () => {
-            const label = screen.getByRole("text", { name: "Password" });
-            expect(label).toBeOnTheScreen();
-        });
+  });
+  describe("Sign up Form modal", () => {
+    test("Modal should appear on screen when submit button is pressed", async () => {
+      const submitbtn = screen.getByTestId("submit-button");
+      const usernameField = screen.getByLabelText("Username text input");
+      const passwordField = screen.getByLabelText("Password input");
+      const user = userEvent.setup();
+
+      await user.type(usernameField, "Ahmed1");
+      await user.type(passwordField, "mypassword");
+      await user.press(submitbtn);
+
+      const modal = screen.getByTestId("form-modal");
+
+      expect(modal).toBeOnTheScreen();
     });
-    describe("Sign up Form modal", () => {
-        test("Modal should appear on screen when submit button is pressed", async () => {
-            const submitbtn = screen.getByTestId("submit-button");
-            const usernameField = screen.getByLabelText("Username text input");
-            const passwordField = screen.getByLabelText("Password input");
-            const user = userEvent.setup();
+    test("Clicking on 'Sign in' when the modal is open, redirects to the sign in form", async () => {
+      renderRouter(
+        {
+          "sign-up": () => <SignUpForm />,
+        },
+        {
+          initialUrl: "/sign-up",
+        },
+      );
+      const submitbtn = screen.getByTestId("submit-button");
+      const usernameField = screen.getByLabelText("Username text input");
+      const passwordField = screen.getByLabelText("Password input");
+      const user = userEvent.setup();
 
-            await user.type(usernameField, "Ahmed1");
-            await user.type(passwordField, "mypassword");
-            await user.press(submitbtn);
+      await user.type(usernameField, "Ahmed1");
+      await user.type(passwordField, "mypassword");
+      await user.press(submitbtn);
+      await user.press(screen.getByText("Sign in"))
 
-            const modal = screen.getByTestId("form-modal");
-
-            expect(modal).toBeOnTheScreen();
-        });
-        test("Clicking on 'Sign in' when the modal is open, redirects to the sign in form", async () => {
-            const submitbtn = screen.getByTestId("submit-button");
-            const usernameField = screen.getByLabelText("Username text input");
-            const passwordField = screen.getByLabelText("Password input");
-            const user = userEvent.setup();
-
-            await user.type(usernameField, "Ahmed1");
-            await user.type(passwordField, "mypassword");
-            await user.press(submitbtn);
-
-            const modal = screen.getByTestId("form-modal");
-            await user.press(screen.getByRole("link"));
-
-            expect(screen).toHavePathname("/sign-in");
-        });
+      expect(screen).toHavePathname("/sign-in");
     });
+  });
 });
--- frontend/app/components/Modal.tsx
+++ frontend/app/components/Modal.tsx
@@ -1,34 +1,32 @@
 import { Modal, Text, Pressable, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { Link } from "expo-router";
+import { useRouter } from "expo-router";
 
 type Props = {
-    show: boolean;
-    onClose: () => void;
+  show: boolean;
 };
 
-export default function FormModal({ show, onClose }: Props) {
-    return (
-        <>
-            <Modal
-                transparent={true}
-                visible={show}
-                onRequestClose={() => {
-                    show = !show;
-                }}
-                testID="form-modal"
-            >
-                <View className="bg-[#e3a76a] h-140 w-268 m-2">
-                    <Text className="font-serif text-sm font-bold">
-                        Thank you for signing up
-                    </Text>
-                    <Link href="/sign-in" asChild>
-                        <Pressable onPress={onClose}>
-                            <Text className="font-serif text-sm font-bold">Sign in</Text>
-                        </Pressable>
-                    </Link>
-                </View>
-            </Modal>
-        </>
-    );
+export default function FormModal({ show }: Props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Modal
+        transparent={true}
+        visible={show}
+        onRequestClose={() => {
+          show = !show;
+        }}
+        testID="form-modal"
+      >
+        <View className="bg-[#e3a76a] h-140 w-268 m-2">
+          <Text className="font-serif text-sm font-bold">
+            Thank you for signing up
+          </Text>
+          <Pressable onPress={() => router.push("/sign-in")}>
+            <Text className="font-serif text-sm font-bold">Sign in</Text>
+          </Pressable>
+        </View>
+      </Modal>
+    </>
+  );
 }
