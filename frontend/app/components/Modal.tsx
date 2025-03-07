106d552c81d95b96567b51a914090db9e3635f9b
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-03-07 16:52:08 +0000

    Frontend - styled form modal

--- frontend/app/components/Modal.tsx
+++ frontend/app/components/Modal.tsx
@@ -18,12 +18,12 @@ export default function FormModal({ show }: Props) {
         }}
         testID="form-modal"
       >
-        <View className="bg-[#e3a76a] h-140 w-268 m-2">
-          <Text className="font-serif text-sm font-bold">
+        <View className="bg-[#e3a76a] h-[140px] w-[268px] m-auto rounded-[10px]">
+          <Text className="font-serif text-lg font-bold m-12 text-center">
             Thank you for signing up
           </Text>
-          <Pressable onPress={() => router.push("/sign-in")}>
-            <Text className="font-serif text-sm font-bold">Sign in</Text>
+          <Pressable onPress={() => router.push("/sign-in")} className="absolute right-0 bottom-0 rounded-[6px] bg-[#c4574f40] p-1 m-2">
+            <Text className="font-serif font-bold">Sign in</Text>
           </Pressable>
         </View>
       </Modal>
